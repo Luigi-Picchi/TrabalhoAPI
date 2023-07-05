92aed8572c85bf57f926d8f25a2871810791cf65
Luigi Castoldi Picchi <125413855+Luigi-Picchi@users.noreply.github.com>
2023-07-05 14:44:22 -0300

    Realizando as alterações

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,15 +5,25 @@ namespace App\Http\Controllers;
 use App\Models\task;
 use App\Http\Requests\StoretaskRequest;
 use App\Http\Requests\UpdatetaskRequest;
+use GuzzleHttp\Psr7\Request as Psr7Request;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+use Symfony\Component\Console\Input\Input;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 
 class TaskController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(string $id)
     {
         //
+        $task = task::find($id);
+        if(!$task){
+            return "inexistente";
+        }
+        return $task
     }
 
     /**
@@ -30,6 +40,23 @@ class TaskController extends Controller
     public function store(StoretaskRequest $request)
     {
         //
+         $task = new task;
+
+        $task->title = $request->input("title");
+        $task->descricao = $request->input("descricao");
+
+        if($request->input("status")== "Completo" || $request->input("status") == "Incompleto"){
+
+            $tarefa->status = $request->input("status");
+            $tarefa->save();
+            return "Cadastrado.";
+
+        }
+        else{
+            return "Status Invalido, colocar completo ou incompleto";
+
+        }
+
     }
 
     /**
@@ -38,14 +65,36 @@ class TaskController extends Controller
     public function show(task $task)
     {
         //
+         $task = task::all();
+        if(!$task){
+            return "inexistente";
+        }
+        return $task;
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(task $task)
+    public function edit(StoretaskRequest $request)
     {
         //
+        $task = task::find($id);
+
+
+        $task->title = $request->input("title");
+        $task->descricao = $request->input("descricao");
+        if($request->input("status")== "Completo" || $request->input("status") == "Incompleto"){
+
+            $task->status = $request->Input("status");
+
+            $task->save();
+            return "Alterado.";
+
+        }
+        else{
+            return "Status Invalido, colocar completo ou incompleto";
+
+        }
     }
 
     /**
@@ -59,8 +108,10 @@ class TaskController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(task $task)
+    public function destroy(string $id)
     {
         //
+        $destroy = tarefa::find($id);
+        $destroy->delete();
     }
 }
